9583f8df680e1b439dcae9e7c01e916e20feff4a
zelmetennani <58562274+zelmetennani@users.noreply.github.com>
2022-07-08 17:42:55 -0500

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,137 @@
+
+
+
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+// Debug
+//const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+
+//const tube = new Math.random() 
+
+const geometry = new THREE.TorusKnotGeometry(3,.35,300,3,8,5)
+
+
+
+// Materials
+
+const material = new THREE.MeshStandardMaterial()
+material.metalness = .7
+material.roughness = .1
+material.color = new THREE.Color(0x455A64)
+
+
+// Mesh
+const sphere = new THREE.Mesh(geometry,material)
+scene.add(sphere)
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xCFD8DC, 6)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = -7
+camera.position.y = 0
+camera.position.z = 15
+scene.add(camera)
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas, 
+    alpha: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+
+document.addEventListener('mousemove', onDocumentMouseMove)
+let mouseX =0;
+let mouseY =0;
+
+let targetX =0;
+let targetY =0;
+
+const windowX = window.innerWidth / 2;
+const windowY = window.innerHeight / 2;
+
+function onDocumentMouseMove(event) {
+    mouseX = (event.clientX - windowX)
+    mouseY = (event.ClientY - windowY)
+}
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    targetX = mouseX * .001
+    targetY = mouseY * .001
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = .5 * elapsedTime
+    sphere.rotation.x = .55 * elapsedTime
+
+    sphere.rotation.y += .5 * (targetX - sphere.rotation.y)
+
+    // Update Orbital Controls
+    // controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
